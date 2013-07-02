5ac7343d4cfeff59b5798b8723c9cd68f7f456c1
Morteza Ipo <mortezaipo@gmail.com>
2013-07-02 12:17:12 +0430

    mongodb added for default database - keygen created

--- ws/ws/smsws.py
+++ ws/ws/smsws.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python
+
 from pyws.server import SoapServer
+from pyws.functions.register import register
+from pymongo.connection import Connection
+
+try:
+    con = Connection('127.0.0.1',27017)
+    db = con.smskeygen
+except:
+    pass #FIXME
 
 server = SoapServer(
     service_name = 'smsws',
@@ -6,12 +16,24 @@ server = SoapServer(
     location = 'http://localhost:8000/api/',
 )
 
-from pyws.functions.register import register
-
 @register()
-def keygen(mobile):
-    pass
-
+def keygen(mobile,char=False):
+    if not mobile.isdigit():
+        return False
+    
+    key_range = map(chr,range(ord('1'),ord('9')))
+    
+    if char:
+        for i in map(chr,xrange(ord('a'),ord('z'))):
+            key_range.append(i)
+            
+    import random
+    
+    random.shuffle(key_range)
+    
+    keygen = random.sample(key_range,6)
+    
+    return ''.join(keygen)
 
 @register()
 def checksent(mobile):
