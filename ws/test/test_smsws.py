1a9d991b9cb22fb356ffe79e65464c0ebe342748
Morteza Ipo <mortezaipo@gmail.com>
2013-07-02 15:34:30 +0430

    unittest of keygen function

--- ws/test/test_smsws.py
+++ ws/test/test_smsws.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python
+
+import unittest
+from ws.smsws import *
+
+class smsws_test(unittest.TestCase):
+    def setUp(self):
+        self.mobile = '09361752529'
+    
+    def test_keygen(self):
+        result = keygen(self.mobile)
+        self.assertTrue(result,'check mobile number')
+
+        result = keygen(self.mobile,True)
+        self.assertTrue(result,'check mobile number')
