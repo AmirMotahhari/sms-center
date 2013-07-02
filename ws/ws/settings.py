e9a90475a35d3af51df83600cabcc998d278ed1a
Morteza Ipo <mortezaipo@gmail.com>
2013-07-02 10:23:32 +0430

    settings file updated

--- ws/ws/settings.py
+++ ws/ws/settings.py
@@ -51,13 +51,6 @@ USE_TZ = True
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '^0er&7nna17+@+v1-e$t!j$y=yr1+ue+kleifumu_89@8&+l%o'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
