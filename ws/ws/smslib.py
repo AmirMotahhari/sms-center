17bf4d30bd7756822d6ae2bba3a5032cdda9766f
Morteza Ipo <mortezaipo@gmail.com>
2013-07-03 11:55:10 +0430

    smslib updated - read settings and connect to details

--- ws/ws/smslib.py
+++ ws/ws/smslib.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from suds.client import Client
+from settings import WEB_SERVICE
+
 
 try:
     con = Client('url://')
@@ -7,10 +9,8 @@ except:
     pass
 
 def send(mobile,message):
-    username = None
-    password = None
-    serviceid = 'free'
-    action = con.service.sendsms(username,password,mobile,message,serviceid)
+    string_args = "%s.%s('%s')" % ('con.service',WEB_SERVICE['service'],"','".join(WEB_SERVICE['argv']).format(message=message,mobile=mobile))
+    action = eval(string_args)
     if action:
         return action
     else:
