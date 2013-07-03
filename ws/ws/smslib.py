df61e8af83d88e84176df45c5c23ce895b834f7a
Morteza Ipo <mortezaipo@gmail.com>
2013-07-03 12:15:06 +0430

    sms lib updated and added steps + connection with argv

--- ws/ws/smslib.py
+++ ws/ws/smslib.py
@@ -2,15 +2,25 @@
 from suds.client import Client
 from settings import WEB_SERVICE
 
+if len(WEB_SERVICE['connect']['argv']):
+    string_args = "('%s','%s')" % (WEB_SERVICE['connect']['url'],"','".join(WEB_SERVICE['connect']['argv']))
+else:
+    string_args = "('%s')" % WEB_SERVICE['connect']['url']
+print string_args
 
 try:
-    con = Client('url://')
+    con = Client(string_args)
 except:
     pass
 
+
 def send(mobile,message):
-    string_args = "%s.%s('%s')" % ('con.service',WEB_SERVICE['service'],"','".join(WEB_SERVICE['argv']).format(message=message,mobile=mobile))
-    action = eval(string_args)
+    for i in WEB_SERVICE:
+        if "step" in i:
+            string_args = "%s.%s('%s')" % ('con.service',WEB_SERVICE[i]['service'],"','".join(WEB_SERVICE[i]['argv']).format(message=message,mobile=mobile))
+            action = eval(string_args)
+            if not action:
+                return False
     if action:
         return action
     else:
