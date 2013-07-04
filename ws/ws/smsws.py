0cfcfce71f65440349ca169455931b8536f7f958
Morteza Ipo <mortezaipo@gmail.com>
2013-07-04 11:37:24 +0430

    logging added for monitoring events

--- ws/ws/smsws.py
+++ ws/ws/smsws.py
@@ -10,8 +10,9 @@ from settings import *
 try:
     con = Connection(DATABASE_HOST,DATABASE_PORT)
     db = con[DATABASE_NAME]
+    logging.info('db:connect: %s:%s' % (DATABASE_HOST,DATABASE_PORT))
 except:
-    logging.error('db connection error')
+    logging.error('db:connect error: %s:%s' % (DATABASE_HOST,DATABASE_PORT))
     sys.exit()
 
 server = SoapServer(
@@ -23,6 +24,7 @@ server = SoapServer(
 @register()
 def keygen(mobile,char=False):
     if not mobile.isdigit():
+        logging.warning("keygen:invalid mobile: %s" % mobile)
         return False
     
     key_range = map(chr,range(ord('1'),ord('9')))
@@ -48,30 +50,37 @@ def keygen(mobile,char=False):
 
     if not row:
         db.users.insert({"mobile":mobile,"key":keygen,"status":"0","send":True,"send_id":send_id},safe=True)
+        logging.info("keygen,db:insert data: %s" % ('-'.join({"mobile":mobile,"key":keygen,"status":"0","send":True,"send_id":send_id})))
     else:
         row['key'] = keygen
         db.users.update({"mobile":mobile},row,safe=True)
+        logging.info("keygen,db:update data: %s" % ('-'.join({"mobile":mobile,"key":keygen})))
     return True
 
 @register()
 def check_sent(mobile):
     row = db.users.find_one({"mobile":mobile})
     if not row:
+        logging.warning("check_sent:not found mobile: %s" % mobile)
         return 3
     if row.get('send'):
+        logging.info("check_sent:success mobile: %s" % mobile)
         return True
     else:
+        logging.info("check_sent:failed mobile: %s" % mobile)
         return False
 
 @register()
 def check_key(mobile,key):
     row = db.users.find_one({"mobile":mobile})
     if not row:
+        logging.warning("check_key:not found mobile: %s" % mobile)
         return 3
     
     if row.get('key') != key:
+        logging.warning("check_key:invalid key: %s mobile: %s" % (key,mobile))
         return False
     
     db.users.remove({"mobile":mobile},safe=True)
-    
+    logging.info("check_key,db:[success key]remove row mobile: %s" % mobile)   
     return True
